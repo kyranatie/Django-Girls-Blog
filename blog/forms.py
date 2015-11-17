06805599ace7522c0360d0755236c271d0fcda86
kyranatie <ndagire.shakira@gmail.com>
2015-11-17 04:38:42 -0500

    Created form and redirect page

---
+++ blog/forms.py
@@ -0,0 +1,9 @@
+__author__ = 'shakira'
+
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ("title","text",)
